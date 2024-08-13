86700b668f4bb64a51abd6e9810d54fe18126ed3
AakashSankarAS <aakashsankar412@gmail.com>
2024-08-13 12:36:32 +0530

    aakash

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get("/api/zoho", ensureAccessToken, async (req, res) => {
 
 app.post("/embedded", async (req, res) => {
   const { meetingKey, encryptPwd } = req.body;
-  const url = `https://meeting.zoho.in/meeting/login/embed-join-meeting.jsp?name=Aakash&key=1374315143&t=572ca5a6d62c1fd4200eae4160fd1e6b24231ba448104adb436a84f6641853be&fdownload=false&srcPPP=false&retryL=false`;
+  const url = `https://meeting.zoho.in/meeting/login/embed-join-meeting.jsp?name=Aakash&key=1374315143&t=572ca5a6d62c1fd4200eae4160fd1e6b24231ba448104adb436a84f6641853be`;
 
   console.log("Requesting URL:", url);
 
