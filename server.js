4b2b3365f1061d9f9a94a55c368cd7b9bdf36fad
AakashSankarAS <aakashsankar412@gmail.com>
2024-08-13 11:56:28 +0530

    Aakash

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post("/embedded", async (req, res) => {
   const { meetingKey, encryptPwd } = req.body;
   try {
     const response = await axios.get(
-      `https://meeting.zoho.in/meeting/login/embedmeeting.jsp?meetingKey=${meetingKey}&newWindow=true&t=${encryptPwd}`
+      `https://meeting.zoho.in/meeting/login/join.jsp?key=1351848187&name=Aakash&viewer=html5&join=true&t=d8e528b4a3aae6d8558062b6fa17a042f4ca591acab07a5443650b67b0b4685f`
     );
     res.send(response.data);
   } catch (error) {
