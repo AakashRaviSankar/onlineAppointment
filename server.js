f0b88bae5939e5c0d5a5c060ac70f25e7eaa328e
AakashSankarAS <aakashsankar412@gmail.com>
2024-09-12 10:11:56 +0530

    new

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.post("/parentForm", async (req, res) => {
     );
     res.send(response.data);
   } catch (error) {
-    console.error("Error posting to parentform", error.message);
+    res.status(400).send(error.message);
 
     res.status(500).send("Error posting form data");
   }
