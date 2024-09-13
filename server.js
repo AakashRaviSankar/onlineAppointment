bde5d08a8992298855c275c361d845b08f3ccb18
AakashSankarAS <aakashsankar412@gmail.com>
2024-09-13 16:09:00 +0530

    service provider

--- server.js
+++ server.js
@@ -125,8 +125,15 @@ app.post("/serviceProvider", async (req, res) => {
 
     res.send(response.data);
   } catch (error) {
-    console.error("Error posting to /enrollement/store:", error.message);
-    res.status(500).send("Error posting form data");
+    if (error.response && error.response.status === 400) {
+      res.status(400).send({
+        message:
+          "The provided email address has already been registered. Please log in using this email or register with a different email address.",
+        code: 400,
+      });
+    } else {
+      res.status(500).send("Error posting form data");
+    }
   }
 });
 
